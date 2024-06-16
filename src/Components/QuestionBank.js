cf903a6f95d1764e28a374d41698b3e69d6d26d4
Navya Manjari <navyamanjari.hyd@gmail.com>
2024-06-16 15:38:18 +0530

    Adding files

---
+++ src/Components/QuestionBank.js
@@ -0,0 +1,40 @@
+// QuestionBank.js
+
+const qBank = [{
+        id: 1,
+        question: "Which province in Canada is the smallest in land size?",
+        options: [
+            "Prince Edward Island",
+            "Nova Scotia",
+            "New Foundland and Labrador",
+            "Yukon territory"
+        ],
+        answer: "Prince Edward Island"
+    },
+    {
+        id: 2,
+        question: "What is Canada’s most populated city?",
+        options: ["Corner Brook", "Ottawa", "Toronto", "Quebec City"],
+        answer: "Toronto"
+    },
+    {
+        id: 3,
+        question: "What landform sits between British Columbia and Alberta?",
+        options: ["Rocky Mountains", "Canadian Shield", "Great Lakes", "Prairies"],
+        answer: "Rocky Mountains"
+    },
+    {
+        id: 4,
+        question: "Canada’s population is around:",
+        options: ["15 million", "28 million", "37 million", "40 million"],
+        answer: "40 million"
+    },
+    {
+        id: 5,
+        question: "What is Canada’s westernmost province?",
+        options: ["Manitoba", "Alberta", "British Columbia", "Saskatchewan"],
+        answer: "British Columbia"
+    }
+];
+
+export default qBank;
